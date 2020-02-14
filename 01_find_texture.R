133bab2a98c09dad18fa61574d1700389068c321
vlesdepo <vlesdepo@bk.ru>
2020-02-14 13:30:58 +0300

    01_find_texture Скрипт поиска повторяющихся линий в картинке переделан на метод Евклидова расстояния. По первой тестовой картинке результат положительный.

--- 01_find_texture.R
+++ 01_find_texture.R
@@ -1,17 +1,23 @@
+#поиск по кратинке повторяющихся строк методом евклидова расстояния
+#побочный эффект: если в картинке строк нет, то в результате будут выданы соседние номера линий (например 23,24) 
+
 #библиотеки
 library(OpenImageR)
 
 #параметры
-folderInput <- "Data"
+folderInput <- "Data" #она же и для выходных файлов
 
 #функции
-dupRows <- function(imag, byrow=1){
-     #ищет строки/столбцы вматрице изображения сумма ркостей которых совпадает
-     # byrow = 1 или 2, согласно нотации функции apply 
-          
-     hashrow <- apply(img,byrow,sum)
-     df <- as.data.frame(table(hashrow))
-     return(which(df$Freq > 1))
+dupRows <- function(imag){
+     #v2 ищет строки/столбцы вматрице изображения сумма ркостей которых совпадает
+ 
+     dfm <- as.data.frame(imag)
+     di <- dist(dfm,method = "euclidean")
+     di <- as.matrix(di)
+     di[which(di==0)] <- NA
+     m <- which(di==min(di,na.rm = T),arr.ind = T)
+     m <- as.data.frame(m)
+     return(m$row)
 }
 
 addLine <- function(imag, veclines, byrow=1){
@@ -29,10 +35,19 @@ addLine <- function(imag, veclines, byrow=1){
 fls <- list.files(folderInput,recursive = T,include.dirs = T,pattern = "*.jpg*",full.names = T)
 
 #обработка
-for (f in fls[1]){
+for (f in fls){
      img <- readImage(f)
-     dr <- dupRows(img)
-     img1 <- addLine(img,dr)
-     writeImage(img1,paste0(f,"_01_test.jpg"))
+     imgG <- rgb_2gray(img)
+     dr <- dupRows(imgG)
+     cat("Горизонтальная обработка файла: ",f,". Найдены строки: ",dr,"\n")
+     img <- addLine(img,dr)
+     dr <- dupRows(t(imgG))
+     cat("Вертикальная обработка файла: ",f,". Найдены строки: ",dr,"\n")
+     img <- addLine(img,dr,byrow = 0)
+     writeImage(img,paste0(f,"_find_texture.jpg"))
 
 }
+
+
+
+
