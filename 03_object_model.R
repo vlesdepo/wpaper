51ec766a4554ac1339d87a8706247aa58828f8d3
vlesdepo <vlesdepo@bk.ru>
2020-02-28 17:51:15 +0300

    2020-02-28 03_object_model Добавлено пару методов по очистке картины и созданию прямоугольников. Рабочие. Проверено.

--- 03_object_model.R
+++ 03_object_model.R
@@ -3,14 +3,28 @@ library(imager)
 
 #конструктор
 wall.create <- function(m,n){
-     r <- cimg(array(1,c(m,n,3,1)))
+     r <- cimg(array(1,c(m,n,1,3)))
      attr(r,"class") <- c("wall",class(r))
      return(r)
 }
 
 wall.clean <- function(w){
-     
-     
+     w[,,,] <- 1
+     attr(w,"class") <- c("wall",class(w))
+     return(w)
 }
+
+wall.addBar <- function(wall,x,y,w,h,col){
+        r <- draw_rect(wall,x,y,x+w,y+h,color=col,filled = T)
+        return(r)
+}
+
+wall.addPicture <- function(wall,x,y,w,h,b,col){
+        
+}
+        
 ######ТЕСТЫ#########
 ww <- wall.create(100,200)
+ww <- wall.clean(ww)
+ww <- wall.addBar(ww,12,12,70,70,col="red")
+save.image(ww,"test.jpg")
