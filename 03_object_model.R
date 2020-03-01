d4ce3a12d88195467cbc79b270b2bd10ecf7ea70
vlesdepo <vlesdepo@bk.ru>
2020-03-01 23:45:14 +0300

    2020-03-01 Некорректно работает ф-я wall.addRect imsub неправильно берет фрагмент изображения.

--- 03_object_model.R
+++ 03_object_model.R
@@ -19,12 +19,26 @@ wall.addBar <- function(wall,x,y,w,h,col){
         return(r)
 }
 
-wall.addPicture <- function(wall,x,y,w,h,b,col){
+wall.addRect <- function(wall,dx,dy,dw,dh,db,col){
+        cat(dx,dy,"\n")
+        cat(dx + db,dx + dw - db,"\n")
+        cat(dy + db, dy + dh - db,"\n")
         
-}
+        t <- imsub(wall, x > (dx + db), x < (dx + dw - db),
+                        y > (dy + db), y < (dy + dh - db))
+        cat(dim(t))
+        cat(dx,dy,"\n")
+        r <- draw_rect(wall,dx,dy,dx+dw,dy+dh,color=col,filled = T)
+        r <- imdraw(r,t,x = (dx + db),
+                        y = (dy + db))
+        
+        return(r)
+} 
         
 ######ТЕСТЫ#########
 ww <- wall.create(100,200)
 ww <- wall.clean(ww)
 ww <- wall.addBar(ww,12,12,70,70,col="red")
-save.image(ww,"test.jpg")
+ww <- wall.addRect(ww,12,12,70,70,3,col="red")
+save.image(ww,"test.png")
+ww1 <- imsub(ww, x > 10, x < 75, y > 10, y < 75)
